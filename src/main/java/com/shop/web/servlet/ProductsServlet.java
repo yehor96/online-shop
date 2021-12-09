92f8ac2c91f29888c44add36cf07fc64fd90e701
yehor96 <yehor.shyriaiev@gmail.com>
2021-12-09 23:12:36 +0200

    add registration button on login page and login info on products page

--- src/main/java/com/shop/web/servlet/ProductsServlet.java
+++ src/main/java/com/shop/web/servlet/ProductsServlet.java
@@ -3,6 +3,7 @@ package com.shop.web.servlet;
 import com.shop.entity.Product;
 import com.shop.service.ProductService;
 import com.shop.web.PageProvider;
+import com.shop.web.handler.UserSessionHandler;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -19,17 +20,22 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class ProductsServlet extends HttpServlet {
 
+    private static final UserSessionHandler USER_SESSION_HANDLER = new UserSessionHandler();
+
     private final ProductService productService;
     private final List<String> tokenStorage;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         List<Product> products = productService.findAll();
-        Map<String, Object> productMap = new HashMap<>();
+        boolean isLoggedIn = USER_SESSION_HANDLER.isLoggedIn(tokenStorage, request);
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("products", products);
+        parameters.put("login", Boolean.toString(isLoggedIn));
 
         try (PrintWriter writer = response.getWriter()) {
-            productMap.put("products", products);
-            writer.println(PageProvider.getPage("products.html", productMap));
+            writer.println(PageProvider.getPage("products.html", parameters));
             response.setContentType("text/html;charset=utf-8");
             response.setStatus(HttpServletResponse.SC_OK);
         }
